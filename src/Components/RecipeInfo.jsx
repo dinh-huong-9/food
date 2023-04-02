35ba4d1b4261c4a784a0ba622385d78357ce8f29
dinh-huong-9 <90088145+dinh-huong-9@users.noreply.github.com>
2023-04-02 16:38:13 +0700

    format

--- src/Components/RecipeInfo.jsx
+++ src/Components/RecipeInfo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-const RecipeInfo=()=>{
-    return(
-        <>
-        <h3>Find your Recipe infomation here</h3>
-        </>
-    )
-}
+const RecipeInfo = () => {
+  return (
+    <>
+      <h3>Find your Recipe infomation here</h3>
+    </>
+  );
+};
 export default RecipeInfo;
