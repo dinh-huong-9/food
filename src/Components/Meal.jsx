c8bc6e82bab61385ae82d210a08fe99ca8ac5bad
dinh-huong-9 <90088145+dinh-huong-9@users.noreply.github.com>
2023-04-02 16:28:34 +0700

    first commit

---
+++ src/Components/Meal.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import MealItem from "./MealItem";
+import RecipeIndex from "./RecipeIndex";
+const Meal=()=>
+{
+    const [url,setUrl]=useState("https://www.themealdb.com/api/json/v1/1/search.php?f=a");
+    const [item,setItem] = useState();
+    const [show,setShow] = useState(false);
+    const [search,setSearch] =useState("");
+    useEffect(()=>{
+        fetch(url).then(res=>res.json()).then(data=> {
+            console.log(data.meals);
+            setItem(data.meals);
+            setShow(true);
+        })
+     },[url])
+    // hien theo chu cai dau
+    const setIndex=(alpha)=>{
+        setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`);
+    }
+    //search
+    const searchRecipe=(evt)=>
+    {
+        if(evt.key==="Enter")
+        {
+            setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+        }
+    }
+    return(
+        <>
+        <div className="main">
+            <div className="heading">
+            <h1>Search Your Food Recipe</h1>
+            <h3>Find delicious recipes and cook them for your family. 
+                There are videos and full recipes to help you cook easily</h3>
+            </div>
+            <div className="searchBox">
+                <input type="search" className="search-bar" 
+                onChange={e=>setSearch(e.target.value)} onKeyPress={searchRecipe}/>
+            </div>
+            <div className="container">
+             
+             {
+                show ? <MealItem data={item}/>:"Not Found"
+             }
+            </div>
+            <div className="indexContainer">
+            <RecipeIndex alphaIndex={(alpha)=>setIndex(alpha)}/>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Meal;
---
+++ src/Components/RecipeInfo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const RecipeInfo=()=>{
+    return(
+        <>
+        <h3>Find your Recipe infomation here</h3>
+        </>
+    )
+}
+export default RecipeInfo;
